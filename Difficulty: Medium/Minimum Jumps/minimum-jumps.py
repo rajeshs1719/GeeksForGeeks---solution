e9ce469f38befe313087a0f0e94aef16024e8a0d
rajeshs1719 <rajeshs01719@gmail.com>
2025-02-01 21:40:51 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Minimum Jumps/minimum-jumps.py
@@ -0,0 +1,40 @@
+#User function Template for python3
+class Solution:
+	def minJumps(self, arr):
+	   # code here
+	   n = len(arr)
+	   if n == 1:
+	       return 0
+	   if arr[0]==0:
+	       return -1
+	   jump = 0
+	   maxReach = arr[0]
+	   steps = arr[0]
+	   for i in range(1,n):
+	       if i == n-1:
+	           return jump+1
+	       maxReach= max(maxReach, i+arr[i])
+	       steps -= 1
+	       if steps==0:
+	           jump +=1
+	           if i>=maxReach:
+	               return -1
+	           steps = maxReach - i
+	   return -1
+	   
+	           
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+if __name__ == '__main__':
+    T = int(input())
+    for i in range(T):
+        # n = int(input())
+        Arr = [int(x) for x in input().split()]
+        ob = Solution()
+        ans = ob.minJumps(Arr)
+        print(ans)
+        print("~")
+# } Driver Code Ends
