cbaa8d8056659f2bbdb7d15350adebe6dec65b89
rajeshs1719 <rajeshs01719@gmail.com>
2025-03-16 00:28:21 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Equilibrium Point/equilibrium-point.py
@@ -0,0 +1,36 @@
+# User function Template for python3
+
+class Solution:
+    def findEquilibrium(self, arr):
+        total_sum = sum(arr) 
+        left_sum = 0  
+        for i in range(len(arr)):
+            right_sum = total_sum - left_sum - arr[i]  
+            if left_sum == right_sum: 
+                return i 
+            left_sum += arr[i]  
+        return -1  
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+import math
+
+
+def main():
+    T = int(input())
+    while (T > 0):
+
+        arr = [int(x) for x in input().strip().split()]
+
+        ob = Solution()
+
+        print(ob.findEquilibrium(arr))
+        print("~")
+        T -= 1
+
+
+if __name__ == "__main__":
+    main()
+
+# } Driver Code Ends
