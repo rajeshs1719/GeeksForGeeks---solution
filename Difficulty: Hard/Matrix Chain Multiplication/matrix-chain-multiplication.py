d26262faadcc689c6212273fce70da532f1d4e32
rajeshs1719 <rajeshs01719@gmail.com>
2025-03-24 23:25:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Matrix Chain Multiplication/matrix-chain-multiplication.py
@@ -0,0 +1,27 @@
+class Solution:
+    def matrixMultiplication(self, arr):
+        n = len(arr)
+        dp = [[0] * n for _ in range(n)]  
+        for l in range(2, n):
+            for i in range(1, n - l + 1):
+                j = i + l - 1
+                dp[i][j] = float('inf')  
+                for k in range(i, j):
+                    cost = dp[i][k] + dp[k+1][j] + (arr[i-1] * arr[k] * arr[j])
+                    dp[i][j] = min(dp[i][j], cost)
+        
+        return dp[1][n-1]
+
+
+#{ 
+ # Driver Code Starts
+# Initial Template for Python 3
+
+t = int(input())  # number of test cases
+for _ in range(t):
+    arr = list(map(int, input().split()))  # input array
+    s = Solution().matrixMultiplication(arr)  # find the missing number
+    print(s)  # print the result
+    print("~")
+
+# } Driver Code Ends
