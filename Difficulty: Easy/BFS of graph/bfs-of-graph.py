cdea0a27cdda188cdb0ccc062c80eca6a41630a4
rajeshs1719 <rajeshs01719@gmail.com>
2025-04-02 23:10:19 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/BFS of graph/bfs-of-graph.py
@@ -0,0 +1,45 @@
+from collections import deque
+
+class Solution:
+    def bfs(self, adj):
+        queue = deque([0]) 
+        visited = set([0]) 
+        result = []  
+        
+        while queue:
+            node = queue.popleft() 
+            result.append(node) 
+            
+            for neighbor in adj[node]:  
+                if neighbor not in visited:
+                    queue.append(neighbor)  
+                    visited.add(neighbor) 
+        return result
+#{ 
+ # Driver Code Starts
+import sys
+
+
+#Position this line where user code will be pasted.
+def main():
+    tc = int(sys.stdin.readline().strip())
+
+    for _ in range(tc):
+        V = int(sys.stdin.readline().strip())  # Number of vertices
+        adj = []  # Adjacency list
+
+        for _ in range(V):
+            input_line = sys.stdin.readline().strip()
+            node = list(map(int, input_line.split())) if input_line else []
+            adj.append(node)
+
+        obj = Solution()
+        ans = obj.bfs(adj)
+        print(" ".join(map(str, ans)))
+        print("~")
+
+
+if __name__ == "__main__":
+    main()
+
+# } Driver Code Ends
